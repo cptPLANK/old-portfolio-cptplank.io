68be94c459f077f76a5d7033f623bc73099814ef
cptPLANK <32358000+cptPLANK@users.noreply.github.com>
2020-03-09 13:13:58 +0100

    'restructure'

--- js/construct.js
+++ js/construct.js
@@ -75,7 +75,7 @@
         };
 
         let response = await getTemplate(json[location].template);
-        addFinalHead(json);
+        addFinalHead(json[location]);
         document.querySelector('main').innerHTML = response;
 
         // let response;
