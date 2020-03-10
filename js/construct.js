c595241f10b50a5ffab7bf6fe1aaedea4cac9c44
cptPLANK <32358000+cptPLANK@users.noreply.github.com>
2020-03-10 16:19:17 +0100

    'restructure'

--- js/construct.js
+++ js/construct.js
@@ -78,7 +78,7 @@
             const jsonCaseStudies = await getJSON('case-studies');
             const caseStudyTemp = await getTemplate('case-studies');
             const advancedTemp = await getTemplate('advanced-content-container');
-            const arrAdvanced = jsonCaseStudies[templateName].content.advancedContent;
+            const arrAdvanced = jsonCaseStudies[templateName].advancedContent;
 
             const getAdvancedContent = arrAdvanced.map(el => replaceTemplate(advancedTemp, arrAdvanced[el])).join();
             console.log(getAdvancedContent);
